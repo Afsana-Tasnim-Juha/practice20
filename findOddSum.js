459140c65e00a156fd998237416119ef827c7516
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-08-06 20:34:18 +0200

    sumofoddnumber

--- findOddSum.js
+++ findOddSum.js
@@ -1,12 +1,35 @@
 function getSumOfAnArray(numbers) {
+    let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
+        sum = sum + element;
 
-        console.log(index, element);
+        //console.log(index, element, sum);
+    } return sum;
+
+}
+
+function getOddNumbersOfAnArray(numbers) {
+    const oddNumbers = [];
+    for (let i = 0; i < numbers.length; i++) {
+        const index = i;
+        const element = numbers[index];
+        if (element % 2 !== 0) {
+            console.log(index, element);
+            oddNumbers.push(element);
+
+
+        }
+
+
     }
+    return oddNumbers;
 
 }
 const myNumbers = [1, 2, 3, 4, 5];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
 
-getSumOfAnArray(myNumbers);
+const oddNumberSum = getSumOfAnArray(oddNumbers);
+console.log(oddNumberSum);
